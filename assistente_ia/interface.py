68b372ae60415f348c0dc5396b46e6e648510d39
Guilherme Assunção <Gui_ab_@hotmail.com>
2026-03-18 11:00:45 -0300

    Update interface.py

--- assistente_ia/interface.py
+++ assistente_ia/interface.py
@@ -1,12 +1,12 @@
 import flet as ft
-from main import processar_comando  # Importa sua lógica
+from main import processar_comando  # lógica
 
 def main(page: ft.Page):
     page.title = "Assistente IA"
     page.vertical_alignment = ft.MainAxisAlignment.START
-    page.theme_mode = ft.ThemeMode.DARK # Fica mais bonito e gasta menos energia
+    page.theme_mode = ft.ThemeMode.DARK # gasta menos energia
     
-    # Função que roda quando clica no botão enviar
+    # roda quando enviar
     def enviar_mensagem(e):
         if campo_texto.value == "":
             return
@@ -15,24 +15,23 @@ def main(page: ft.Page):
         chat_log.controls.append(ft.Text(f"Você: {pergunta}", color="blue"))
         page.update()
         
-        # Chama seu cérebro (vai travar a tela um pouco, depois arrumamos isso)
+        # Chama cérebro (trava a tela um pouco,  arrumar depois isso)
         resposta = processar_comando(pergunta)
         
         chat_log.controls.append(ft.Text(f"IA: {resposta}", color="green", weight="bold"))
         campo_texto.value = ""
         page.update()
 
-    # Elementos da Tela
+    # Tela
     chat_log = ft.ListView(expand=True, spacing=10, auto_scroll=True)
     campo_texto = ft.TextField(hint_text="Fale ou digite algo...", expand=True, on_submit=enviar_mensagem)
-    # Usando string direta (funciona sempre)
     botao_enviar = ft.IconButton(icon="send", on_click=enviar_mensagem)
 
-    # Layout (Uma coluna com o chat em cima e a entrada embaixo)
+    # coluna com o chat em cima e a entrada embaixo
     page.add(
         ft.Container(
             content=chat_log,
-            expand=True, # Ocupa todo o espaço disponível
+            expand=True, # pega todo o espaço 
             padding=10
         ),
         ft.Row(
@@ -44,4 +43,4 @@ def main(page: ft.Page):
 ft.app(target=main)
 
 
-#LIBGL_ALWAYS_SOFTWARE=1 /home/lola/VScode/.libs/bin/python /home/lola/VScode/Bento-XVI/assistente_ia/interface.py
+# NÂO ESQUECER (LIBGL_ALWAYS_SOFTWARE=1 /home/lola/VScode/.libs/bin/python /home/lola/VScode/Bento-XVI/assistente_ia/interface.py)
